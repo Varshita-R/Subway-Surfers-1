7ae2328e5ef7fa0861203bc1e2060f7659f8593a
Varshita-R <86157519+Varshita-R@users.noreply.github.com>
2021-09-21 16:52:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Subway-Surfers-1
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,82 @@
+var backgroundImg;
+var track1, track2, track3;
+var jake, security;
+var jakeImg;
+
+function preload(){
+backgroundImg = loadImage("images/Tracks.png");
+jakeImg = loadImage("images/JakeFront.png");
+}
+
+function setup() {
+
+  createCanvas(windowWidth,windowHeight);
+
+  edges = createEdgeSprites();
+
+  track1 = createSprite(windowWidth/4,height,width,height);
+  track1.scale = 2;
+  track1.addImage("track1",backgroundImg);
+  track1.velocityY = 5;
+
+  track2 = createSprite(windowWidth/2+50,height,width,height);
+  track2.scale = 2;
+  track2.addImage("track2",backgroundImg);
+  track2.velocityY = 5;
+
+  track3 = createSprite(windowWidth/2+500,height,width,height);
+  track3.scale = 2;
+  track3.addImage("track3",backgroundImg);
+  track3.velocityY = 5;
+
+  jake = createSprite(width/2+45,height-200,20,20);
+  jake.scale = 0.2;
+  jake.addImage("jake",jakeImg);
+  //jake.velocityY = -6;
+
+}
+
+function draw() {
+
+  background("grey");
+
+  
+  jake.bounce(edges[0]);
+  jake.bounce(edges[1]);
+
+  if(track1.y > height/2){
+    track1.y = 350;
+  }
+
+  if(track2.y > height/2){
+    track2.y = 350;
+  }
+
+  if(track3.y > height/2){
+    track3.y = 350;
+  }
+
+  if(keyDown("LEFT_ARROW")){
+    jake.x = jake.x-150;
+  }
+
+  if(keyDown("RIGHT_ARROW")){
+    jake.x = jake.x+150;
+  }
+
+  if(keyDown("UP_ARROW")){
+    jake.velocityY = -4;
+  }
+
+  //jake.velocityY = jake.velocityY+0.5;
+
+  if(jake.x < 0){
+    jake.x = 20;
+  }
+
+  if(jake.x > width-20){
+    jake.x = width-20;
+  }
+
+  drawSprites();
+}
